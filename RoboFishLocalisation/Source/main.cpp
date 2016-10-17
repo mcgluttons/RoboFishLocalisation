c85bc521f5933f14f0f5b57ad608dd9c7f9c4ec9
Justin Wolf <Justin Wolf>
2016-10-18 00:08:32 +1100

    Feature Addition: Tracking objects using motion detection by comparing two frames.

--- RoboFishLocalisation/Source/main.cpp
+++ RoboFishLocalisation/Source/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "opencv2\highgui\highgui.hpp"
 #include "opencv2\opencv.hpp"
+#include "opencv2\imgproc.hpp"
 #include "myopencv.h"
 
 void help();
+void localiseFish(cv::Mat);
 
 int main(int argc, char** argv) {
 	help();
@@ -37,11 +39,24 @@ int main(int argc, char** argv) {
 		
 		cv::VideoCapture capture = openVideoCapture(argv[2]);
 
-		cv::Mat frame;
+		cv::Mat frame, diff_frame, thresh_frame;
+		cv::Mat gray_frame1, gray_frame2;
+		createDisplay("Difference Image");
+		createDisplay("Threshold Image");
+		createDisplay("Blurred");
 		for (;;) {
 			capture >> frame;
-			cv::Mat gray_frame;
-			cv::cvtColor(frame, gray_frame, CV_BGR2GRAY);
+			cv::cvtColor(frame, gray_frame1, CV_BGR2GRAY);
+			capture >> frame;
+			cv::cvtColor(frame, gray_frame2, CV_BGR2GRAY);
+			cv::absdiff(gray_frame1, gray_frame2, diff_frame);
+			displayFrame("Difference Image", diff_frame);
+			cv::threshold(diff_frame, thresh_frame, 20, 255, CV_THRESH_BINARY);
+			displayFrame("Threshold Image", thresh_frame);
+			cv::blur(thresh_frame, thresh_frame, cv::Size(10, 10));
+			cv::threshold(thresh_frame, thresh_frame, 20, 255, CV_THRESH_BINARY);
+			displayFrame("Blurred", thresh_frame);
+			localiseFish(thresh_frame);
 		}
 
 	}
@@ -58,4 +73,35 @@ int main(int argc, char** argv) {
 void help() {
 	std::cout << "Please use the following list of commands for the RoboFishLocalistion System." << std::endl;
 	// add all commands with descriptions
+}
+
+void localiseFish(cv::Mat thresh_frame) {
+	cv::Mat local_frame;
+	thresh_frame.copyTo(local_frame);
+
+	std::vector<std::vector<cv::Point>> contours;
+	std::vector<cv::Vec4i> hierarchy;
+
+	cv::findContours(local_frame, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+	if (contours.size() > 0) {
+		std::cout << "contours found!!!" << std::endl;
+		//for (int i = 0; i < contours.size(); i++) {
+		//	std::cout << i << ": " << contours[i] << std::endl;
+		//}
+		
+		std::vector<std::vector<cv::Point>> largestContour;
+		largestContour.push_back(contours.at(contours.size() - 1));
+		//std::cout << largestContour[0] << std::endl;
+		cv::Rect boundingRectangle = cv::Rect(0, 0, 0, 0);
+		boundingRectangle = cv::boundingRect(largestContour.at(0));
+		int xpos = boundingRectangle.x + (boundingRectangle.width / 2);
+		int ypos = boundingRectangle.y + (boundingRectangle.width / 2);
+
+		cv::Scalar color = cv::Scalar(255, 0, 255);
+		cv::rectangle(local_frame, boundingRectangle.tl(), boundingRectangle.br(), color, 2, 8, 0);
+		displayFrame("rectangles", local_frame);
+	}
+
+
 }
